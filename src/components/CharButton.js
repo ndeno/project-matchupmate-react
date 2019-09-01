e5be37832d67ef9d5ecc687d8712e9d4c24c57a3
Ndenn00 <nialdennison00@gmail.com>
2019-09-01 22:11:00 +0100

    test commit 1

--- src/components/CharButton.js
+++ src/components/CharButton.js
@@ -3,25 +3,29 @@ import { Button } from "react-bootstrap";
 
 class CharButton extends Component {
 
-  constructor(props){
+  constructor(props) {
     super(props);
     this.handleClick = this.handleClick.bind(this);
   }
 
-  handleClick(e){
+  handleClick(e) {
     e.preventDefault();
-    this.props.charSelect(e.target.value); 
+    this.props.charSelect(e.target.value);
     this.props.__next();
-  } 
+  }
 
-  render(){
+  render() {
     return (
-     <Button  variant="secondary" className="charbutton-button" onClick={this.handleClick} value={this.props.name} id={this.props.name}>
-      {this.props.name}
-     </Button>
+      <Button variant="secondary"
+        className="charbutton-button"
+        onClick={this.handleClick}
+        value={this.props.name}
+        id={this.props.name}>
+        {this.props.name}
+      </Button>
     );
   }
- 
+
 };
 
 export default CharButton;
